36de8e8872bd3084769710ff9c5ffef37f44f627
duncan4123 <dmgwizz@gmail.com>
2023-06-05 00:33:02 +1000

    update veNFT script to write to file

--- scripts/VeNFTSnapshot.s.sol
+++ scripts/VeNFTSnapshot.s.sol
@@ -8,7 +8,7 @@ import {Script} from "../lib/forge-std/src/Script.sol";
 import {VotingEscrow} from "../contracts/VotingEscrow.sol";
 
 contract VeNFTSnapshot is Script {
-    function run() external view {
+    function run() external  {
         VotingEscrow votingEscrow = VotingEscrow(0x990efF367C6c4aece43c1E98099061c897730F27);
         // From https://alto.build/collections/0x990eff367c6c4aece43c1e98099061c897730f27
         uint256 currentTokenId = 0;
@@ -19,12 +19,12 @@ contract VeNFTSnapshot is Script {
             if (owner != address(0)) {
                 (int128 lockAmount,) = votingEscrow.locked(currentTokenId);
 
-                console2.log("Token ID: ");
-                console2.log(currentTokenId);
-                console2.log("Owner: ");
-                console2.log(owner);
-                console2.log("Locked amount: ");
-                console2.log(lockAmount);
+                vm.writeLine("text.txt", "Token ID: ");
+                vm.writeLine("text.txt", vm.toString(currentTokenId));
+                vm.writeLine("text.txt", "Owner: ");
+                vm.writeLine("text.txt", vm.toString(owner));
+                vm.writeLine("text.txt", "Locked amount: ");
+                vm.writeLine("text.txt", vm.toString(lockAmount));
             }
 
             currentTokenId++;
