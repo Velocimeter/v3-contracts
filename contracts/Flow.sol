e59538fb24bec342143e478740996812a55c08c8
0xmotto <0xmotto@protonmail.com>
2023-07-21 14:00:02 +0800

    fix: amend token name

--- contracts/Flow.sol
+++ contracts/Flow.sol
@@ -5,8 +5,8 @@ import "contracts/interfaces/IFlow.sol";
 
 contract Flow is IFlow {
 
-    string public constant name = "FantomVelociMeter";
-    string public constant symbol = "FVM";
+    string public constant name = "VelociMantle";
+    string public constant symbol = "MVM";
     uint8 public constant decimals = 18;
     uint public totalSupply = 0;
 
