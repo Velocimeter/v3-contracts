95da74809d85b52ae1088fd7edb466ec6e025819
Ceazor <73511897+Ceazor@users.noreply.github.com>
2023-07-22 13:50:02 -0600

    rebated

--- script/Rebater.s.sol
+++ script/Rebater.s.sol
@@ -11,14 +11,19 @@ import {Voter} from "../contracts/Voter.sol";
 contract Rebater is Script { 
 
     // partner destinations
-    address constant DUES = 0x83B285E802D76055169B1C5e3bF21702B85b89Cb;
     address constant MPX = 0xDd257d090FA0f9ffB496b790844418593e969ba6;
-    address constant BAY = 0xa9D3b1408353d05064d47DAF0Dc98E104eb9c98A;
-    address constant FGHST = 0x13757D72FAc994F9690045150d60929D64575843; 
+    address constant DEUS = 0x83B285E802D76055169B1C5e3bF21702B85b89Cb;
     address constant LQDR = 0x06917EFCE692CAD37A77a50B9BEEF6f4Cdd36422;
     address constant TAROT = 0x5F21E3cA21fc0C33cfA5FB33fc7031f61e34D256;
-    address constant GRAIN = 0xf2671D5c19C479Cd68b1FE6d2f3A1e0CC7Fe4Ad4;
+    address constant SCREAM = 0x89955a99552F11487FFdc054a6875DF9446B2902;
+    address constant BAY = 0xa9D3b1408353d05064d47DAF0Dc98E104eb9c98A;
     address constant ETHOS =  0xf2671D5c19C479Cd68b1FE6d2f3A1e0CC7Fe4Ad4;
+    address constant FUCKMULTI = 0x13483374E385ACbEB8285954D0A1c61b0b9a2f62;
+    address constant GRAIN = 0xf2671D5c19C479Cd68b1FE6d2f3A1e0CC7Fe4Ad4;
+    address constant FRAX = 0xE838c61635dd1D41952c68E47159329443283d90;
+    address constant XEX = 0x89DdBAcc77A14D505101CE669a683e1B01781701;
+    address constant BLOTR = 0x58328aE00df6017Dbe83c5F59CaB96430E6926Ae; 
+    address constant FGHST = 0x13757D72FAc994F9690045150d60929D64575843; 
 
     address constant MintTankAddy = 0x14Dc007573Ac5dCC94410bc29DCBb4923e54C69d;
     uint256 constant FULL_LOCK = 52 * 7 * 86400;
@@ -32,14 +37,20 @@ contract Rebater is Script {
 
             MintTank minttank = MintTank(MintTankAddy);
 
-            minttank.mintFor(2099 * 1e18, FULL_LOCK, DUES);
-            minttank.mintFor(2002 * 1e18, FULL_LOCK, MPX);
-            minttank.mintFor(1251 * 1e18, FULL_LOCK, BAY);
-            minttank.mintFor(1232 * 1e18, FULL_LOCK, FGHST);
-            minttank.mintFor(1057 * 1e18, FULL_LOCK, LQDR);
-            minttank.mintFor(257 * 1e18, FULL_LOCK, TAROT);
-            minttank.mintFor(464 * 1e18, FULL_LOCK, GRAIN);
-            minttank.mintFor(726 * 1e18, FULL_LOCK, ETHOS);
+            minttank.mintFor(3349 * 1e18, FULL_LOCK, MPX);
+            minttank.mintFor(2792 * 1e18, FULL_LOCK, DEUS);
+            minttank.mintFor(1872 * 1e18, FULL_LOCK, LQDR);
+            minttank.mintFor(1661 * 1e18, FULL_LOCK, TAROT);
+            minttank.mintFor(1021 * 1e18, FULL_LOCK, SCREAM);
+            minttank.mintFor(961 * 1e18, FULL_LOCK, BAY);
+            minttank.mintFor(771 * 1e18, FULL_LOCK, ETHOS);
+            minttank.mintFor(193 * 1e18, FULL_LOCK, FUCKMULTI);
+            minttank.mintFor(504 * 1e18, FULL_LOCK, GRAIN);
+            minttank.mintFor(503 * 1e18, FULL_LOCK, FRAX);
+            minttank.mintFor(67 * 1e18, FULL_LOCK, XEX);
+            minttank.mintFor(215 * 1e18, FULL_LOCK, BLOTR);
+
+
 
             // minttank.transferOwnership(MSIG);
 
