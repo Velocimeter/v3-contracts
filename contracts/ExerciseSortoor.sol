8f38803e6aab3f6ca715b930ad4372dd187446c4
Ceazor <73511897+Ceazor@users.noreply.github.com>
2023-08-31 20:21:40 -0600

    automate the FVM buyback

---
+++ contracts/ExerciseSortoor.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import 'openzeppelin-contracts/contracts/utils/math/Math.sol';
+import 'contracts/interfaces/IERC20.sol';
+import 'contracts/interfaces/IRouter.sol';
+import 'contracts/interfaces/IVeFlowBooster.sol';
+import 'openzepplin-contracts/contracts/access/Ownable.sol';
+
+// Bribes pay out rewards for a given pool based on the votes that were received from the user (goes hand in hand with Voter.vote())
+contract ExerciseSortoor is Ownable{
+    address public treasury;
+    address public veBooster;
+    address public router;
+    uint256 public ratio = 80; // actual % of how much wftm will swap for FVM
+
+    address public FVM = 0x07BB65fAaC502d4996532F834A1B7ba5dC32Ff96;
+    address public wFTM = 0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83;
+
+    constructor(address _treasury, address _veBooster, address _router) {
+        treasury = _treasury;
+        veBooster = _veBooster;
+        router = _router;
+        giveAllowances();
+    }
+
+// ADMINS Set Functions
+    function setTreasury(address _treasury) external onlyOwner {
+        require (_treasury != 0);
+        treasury = _treasry;
+    }
+    function setVeBooster(address _booster) external onlyOwner {
+        require (_booster != 0);
+        veBooster = _booster;
+    }
+    function setRouter(address _router) external onlyOwner {
+        require (_router != 0);
+        router = _router;
+    }
+    function setRatio(uint256 _ratio) external onlyOwner {
+        require (_treasury != 0); //<-------------?????
+        ratio = _ratio;
+    }
+// Public Functions
+    function balanceOfFVM() public view returns (uint){
+        return IERC20(FVM).balanceOf(address(this));
+    }
+    function balanceOfWFTM() public view returns (uint){
+        return IERC20(wFTM).balanceOf(address(this));
+    }
+    function disperse() public {
+        uint256 wftmBal = balanceOfWFTM();
+        uint256 wftmToSwap = wftmBal * ratio / 100;
+        IRouter(router).swapExactTokensForTokensSimple(wftmToSwap, 1, wFTM, FVM, false, address(this), block.timestamp);
+        uint256 wftmBalNow = balanceOfWFTM();
+        IERC20(wFTM).transfer(treasury, wftmBalNow);
+        uint256 FVMBal = balanceOfFVM();
+        IVeFlowBooster(veBooster).donateFlow(FVMBal);        
+    }
+
+// Admin Safety Functions
+    function inCaseTokensGetStuck(address _token, address _to) external onlyOwner {
+        uint256 amount = IERC20(_token).balanceOf(address(this));
+        IERC20(_token).transfer(_to, amount);
+    }
+    function giveAllowances() external onlyOwner {
+        IERC20(FVM).approve(veBooster, type(uint256).max);
+        IERC20(wFTM).approve(router, type(uint256).max);
+    }
+    function removeAllowances() external onlyOwner {
+        IERC20(FVM).approve(veBooster, 0);
+        IERC20(wFTM).approve(router, 0);
+    }
+}
---
+++ contracts/interfaces/IVeFlowBooster.sol
@@ -0,0 +1,5 @@
+pragma solidity 0.8.13;
+
+interface IVeFlowBooster {
+    function donateFlow(uint256 _amount) external;
+}
