106f196f064b134d1e8698aa209540c8a3bdba7c
dawid.d <dawid.d@dawid.d.com>
2023-12-15 10:52:44 +0100

    CarbonPair.sol - remove the maturity, - re-write of the withdraw function to not close strategy and withdraw only user share - add new init function to allow to copy in-trade strategy like CL

--- contracts/carbon/CarbonPair.sol
+++ contracts/carbon/CarbonPair.sol
@@ -11,9 +11,6 @@ import 'contracts/interfaces/carbon/ICarbonController.sol';
 
 contract CarbonPair is ERC20,ReentrancyGuard,IERC721Receiver{
 
-    // The timestamp which users can withdraw their position from carbon
-    uint256 public maturity;
-
     //curent id of the strategy in carbon 
     uint256 public strategyId;
 
@@ -27,10 +24,40 @@ contract CarbonPair is ERC20,ReentrancyGuard,IERC721Receiver{
     //errors
     error SlippageTooHigh();
 
-    constructor(string memory _name, string memory _symbol, uint256 _maturity,address _carbonController) ERC20(_name,_symbol) {
-        maturity = _maturity;
+    constructor(string memory _name, string memory _symbol,address _carbonController) ERC20(_name,_symbol) {
         carbonController = _carbonController;
         initiated = false;
+    }
+
+    function initStrategyExactCopy(uint256 _strategyIdToCopy) public {
+        require(!initiated, "vault started");
+ 
+        Strategy memory strategyToCopy = ICarbonController(carbonController).strategy(_strategyIdToCopy);
+         
+        token0 = Token.unwrap(strategyToCopy.tokens[0]);
+        token1 = Token.unwrap(strategyToCopy.tokens[1]);
+
+        Order memory token0Order = strategyToCopy.orders[0]; 
+        Order memory token1Order = strategyToCopy.orders[1];
+
+        uint128 amountToken0 =  token0Order.y;
+        uint128 amountToken1 =  token1Order.y;
+
+        if(amountToken0 > 0)
+            SafeERC20.safeTransferFrom(IERC20(token0), msg.sender, address(this), amountToken0);
+        
+        if(amountToken1 > 0)
+            SafeERC20.safeTransferFrom(IERC20(token1), msg.sender, address(this), amountToken1);
+
+        SafeERC20.safeApprove(IERC20(token0), carbonController, amountToken0);
+        SafeERC20.safeApprove(IERC20(token1), carbonController, amountToken1);
+
+        strategyId = ICarbonController(carbonController).createStrategy(strategyToCopy.tokens[0], strategyToCopy.tokens[1], [token0Order,token1Order]);
+
+        uint _amount18Shares = amountToken0 > amountToken1 ? _to18decimals(token0,amountToken0) : _to18decimals(token1,amountToken1);
+        _mint(msg.sender, _amount18Shares); // this i start share by defult is base on the amout of token that has higher vaule (first init is 100% of shares)
+
+        initiated = true;
     }
 
     function initStrategy(uint256 _strategyIdToCopy, uint128 _amountToken0,uint128 _amountToken1) public {
@@ -68,18 +95,8 @@ contract CarbonPair is ERC20,ReentrancyGuard,IERC721Receiver{
         initiated = true;
     }
 
-    function closeStrategy() public {
-        require(block.timestamp >= maturity, "vault has not matured");
-
-        if(strategyId != 0) {
-            ICarbonController(carbonController).deleteStrategy(strategyId);
-            strategyId = 0;
-        }
-    }
-
     function deposit(address tokenToDeposit,uint128 _amount,uint128 _maxAmountSecondToken) public nonReentrant {
         require(strategyId != 0, "vault not started");
-        require(block.timestamp < maturity, "vault has matured");
         require(tokenToDeposit == token0 || tokenToDeposit == token1, "deposit token is not part of the vault");
         require(_amount > 0, "_amount == 0");
 
@@ -143,22 +160,41 @@ contract CarbonPair is ERC20,ReentrancyGuard,IERC721Receiver{
     }
 
     function withdraw(uint256 _shares) public nonReentrant{
-        closeStrategy();
+        require(_shares > 0, "_shares == 0");
         
-        // withdraw deposit
-        uint token0Amount = (balanceOfToken0() * _shares) / totalSupply();
-        
-        // withdraw executed ammounts 
-        uint token1Amount = (balanceOfToken1() * _shares) /  totalSupply();
-
         _burn(msg.sender, _shares);
 
+        Strategy memory strategy = ICarbonController(carbonController).strategy(strategyId);
+
+        uint128 token0Amount = uint128((strategy.orders[0].y * _shares) /  totalSupply());
+        uint128 token1Amount = uint128((strategy.orders[1].y * _shares) /  totalSupply());
+
+        require(token0Amount > 0 || token1Amount > 0,"0 to withdraw"); // in case if sombody try to burn small ammount that are lost in the uint256 -> uint128 convert
+
+        Order memory newOrder0;
+        
+        newOrder0.A = strategy.orders[0].A;
+        newOrder0.B = strategy.orders[0].B;
+
+        newOrder0.z = ((strategy.orders[0].y - token0Amount) *  strategy.orders[0].z ) / strategy.orders[0].y;
+        newOrder0.y = strategy.orders[0].y - token0Amount;
+        
+        Order memory newOrder1; 
+
+        newOrder1.A = strategy.orders[1].A;
+        newOrder1.B = strategy.orders[1].B;
+      
+        newOrder1.z = ((strategy.orders[1].y - token1Amount) *  strategy.orders[1].z ) / strategy.orders[1].y;
+        newOrder1.y = strategy.orders[1].y - token1Amount;
+
+        ICarbonController(carbonController).updateStrategy(strategyId, strategy.orders, [newOrder0,newOrder1]);
+
         if(token0Amount > 0) {
-            SafeERC20.safeTransfer(IERC20(token0), msg.sender, token0Amount);
+            SafeERC20.safeTransfer(IERC20(Token.unwrap(strategy.tokens[0])), msg.sender, token0Amount);
         }
 
         if(token1Amount > 0) {
-            SafeERC20.safeTransfer(IERC20(token1), msg.sender, token1Amount);
+            SafeERC20.safeTransfer(IERC20(Token.unwrap(strategy.tokens[1])), msg.sender, token1Amount);
         }
 
     }
